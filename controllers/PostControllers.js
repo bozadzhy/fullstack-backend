d82e4351ed58285555057c975eb661de60bf412a
FedorBoz <bozadzhy.f@gmail.com>
2024-09-24 10:57:58 +0300

    m

---
+++ controllers/PostControllers.js
@@ -0,0 +1,109 @@
+import PostModel from "../models/Post.js";
+
+export const getAll = async (req, res) => {
+  try {
+    const posts = await PostModel.find().populate("user").exec(); // обьединяем пости и создателя поста с помощью populate().exec()
+    res.json(posts);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "не удалось получить статьи",
+    });
+  }
+};
+
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id; // достаем динамический параметр id из запроса
+
+    const updatedPost = await PostModel.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        $inc: { viewsCount: 1 }, // то что ми будем оновлять (просмотри)
+      },
+      {
+        returnDocument: "after", // вернуть обновленній документ
+      }
+    );
+    if (!updatedPost) {
+      return res.status(404).json({
+        message: "статья не найдена",
+      });
+    }
+    res.json(updatedPost);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: "не удалось получить статью",
+    });
+  }
+};
+
+export const remove = async (req, res) => {
+  try {
+    const postId = req.params.id; // достаем динамический параметр id из запроса
+    const deletedPost = await PostModel.findOneAndDelete({
+      _id: postId,
+    });
+    if (!deletedPost) {
+      return res.status(404).json({
+        message: "статья не найдена",
+      });
+    }
+    res.json({
+      success: true,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: "не удалось удалить статью",
+    });
+  }
+};
+
+export const create = async (req, res) => {
+  try {
+    const doc = new PostModel({
+      title: req.body.title,
+      text: req.body.text,
+      imageUrl: req.body.imageUrl,
+      tags: req.body.tags,
+      user: req.userId,
+    });
+    const post = await doc.save();
+    res.json(post);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "не удалось создать статью",
+    });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    await PostModel.updateOne(
+      {
+        _id: postId,
+      },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        tags: req.body.tags,
+        user: req.userId,
+      }
+    );
+    res.json({
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "не удалось обновить статью",
+    });
+  }
+};
---
+++ controllers/UserController.js
@@ -0,0 +1,116 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import UserModel from "../models/User.js";
+
+
+export const register = async (req, res) => {
+  try {
+    const password = req.body.password;
+    //соль- алгоритм шифрования
+    const solt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, solt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatar,
+      passwordHash: hash,
+    });
+    // сохраняем юзера в монгоДБ
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+    // деструктуризация - вытаскиваем из юзера пароль (он не нужен)) а всё остальное в userData  и его в кидаем в ответ
+    const { passwordHash, ...userData } = user._doc;
+
+    // возвращаем инфо о пользователе
+    res.json({
+      ...userData,
+      token: token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: " не удалось зарегистрировать юзера",
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    // находим юзера по емайлу
+    const user = await UserModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).json({
+        message: "такого юзера нет",
+      });
+    }
+    // сравниваем его пароль с зашифрованным
+    const isValidPassword = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+    if (!isValidPassword) {
+      return res.status(404).json({
+        message: "неверный логин или пароль",
+      });
+    }
+    // если всё ок. создаем новый токен
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+    // деструктуризация - вытаскиваем из юзера пароль (он не нужен)) а всё остальное в userData  и его в кидаем в ответ
+    const { passwordHash, ...userData } = user._doc;
+
+    // возвращаем инфо о пользователе
+    res.json({
+      ...userData,
+      token: token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: " не удалось авторизоваться",
+    });
+  }
+};
+
+export const getMe = async (req, res) => {
+  try {
+    // Проверка на наличие идентификатора пользователя
+    if (!req.userId) {
+      return res.status(400).json({
+        message: "Некорректный токен",
+      });
+    }
+
+    const user = await UserModel.findById(req.userId);
+    if (!user) {
+      return res.status(404).json({
+        message: "Пользователь не найден",
+      });
+    }
+
+    const { passwordHash, ...userData } = user._doc;
+    res.json(userData);
+  } catch (err) {
+    console.error("Ошибка при получении пользователя:", err);
+    res.status(500).json({
+      message: "Нет доступа",
+    });
+  }
+};
